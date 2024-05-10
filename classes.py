3e3c52c981682e25a5a2ffcc37531ef2f0666e87
Vikenty Bobrov <keshabobrov@gmail.com>
2024-05-10 18:07:54 +0300

    - Added class method for parsing members list

--- classes.py
+++ classes.py
@@ -6,8 +6,8 @@ import json
 class Groups:
     def __init__(self, group_id) -> None:
         self.group_id = group_id
-        self.group_name = None
-        self.group_members_count = None
+        self.name = None
+        self.members_count = None
         self.is_verified = None
         self.type = None
     
@@ -21,7 +21,25 @@ class Groups:
         }
         response = requests.post(f"{variables.url}/{api_method}", request)
         json_response = json.loads(response.text)
-        self.group_members_count = json_response['response']['groups'][0]['members_count']
-        self.group_name = json_response['response']['groups'][0]['name']
+        self.members_count = json_response['response']['groups'][0]['members_count']
+        self.name = json_response['response']['groups'][0]['name']
         self.is_verified = json_response['response']['groups'][0]['verified']
         self.type = json_response['response']['groups'][0]['type']
+    
+    def get_members_list(self):
+        offset = 0
+        members_list = []
+        api_method = 'groups.getMembers'
+        get_members_list_request ={
+            'access_token': variables.token,
+            'group_id': self.group_id,
+            'v': variables.version
+        }
+        while offset < self.members_count:
+            get_members_list = f"{variables.url}/{api_method}"
+            get_members_list_request['offset'] = offset
+            response = requests.post(get_members_list, get_members_list_request)
+            json_response = json.loads(response.text)
+            members_list.append(json_response['response']['items'])
+            offset += 1000
+        return members_list
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import csv
-import datasetCollector
 import classes
 
 
@@ -11,7 +10,7 @@ def main():
     print('Worker start')
     group = classes.Groups(id)
     group.get_group_basic_info()
-    members_list = datasetCollector.get_members_list(group.group_id, group.group_members_count)
+    members_list = group.get_members_list()
     with open(f'datasets/{group_name}.csv', 'w') as csv_file:
         writer = csv.writer(csv_file, delimiter='\n')
         writer.writerows(members_list)
