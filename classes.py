e5792456ca3534d2d81d81167437492e61714a23
Vikenty Bobrov <keshabobrov@gmail.com>
2024-05-11 21:25:23 +0300

    - Added support for multiple-values fields - Added headers to csv - Fixed issue with API ban - Fixed issue with incorrect users parsing - Updated collected datasets

--- classes.py
+++ classes.py
@@ -3,6 +3,7 @@ import requests
 import json
 import time
 import csv
+import os
 
 
 class Groups:
@@ -23,12 +24,15 @@ class Groups:
         }
         response = requests.post(f"{variables.url}/{api_method}", request)
         json_response = json.loads(response.text)
+        # Fetching basic group info
         self.members_count = json_response['response']['groups'][0]['members_count']
         self.name = json_response['response']['groups'][0]['name']
         self.is_verified = json_response['response']['groups'][0]['verified']
         self.type = json_response['response']['groups'][0]['type']
     
     def get_members_list(self):
+        # Getting a 1000 members id's and providing them to get_user_info function for further parsing
+        # Repeating until last thousand will be processed
         offset = 0
         members_list = []
         api_method = 'groups.getMembers'
@@ -43,7 +47,8 @@ class Groups:
             response = requests.post(get_members_list, get_members_list_request)
             json_response = json.loads(response.text)
             members_list.append(json_response['response']['items'])
-            get_user_info(','.join(map(str,members_list[0])))
+            get_user_info(','.join(map(str,json_response['response']['items'])), self.name)
+            time.sleep(3)
             offset += 1000
         return members_list
 
@@ -65,28 +70,26 @@ class Users:
         self.personal_langs = None
         self.personal_religion = None
         self.sex = None
-        self.status = None
         self.university_name = None
         self.verified = None
         
     def attr_to_list(self):
+        # Adding all class attributes to list for further csv saving
         attr_list = []
         attr_list.extend([self.id, self.is_closed, self.bdate, self.career, self.city, self.followers_count, self.education])
         attr_list.extend([self.interests, self.last_seen_time, self.last_seen_platform, self.occupation_type])
-        attr_list.extend([self.personal_political, self.personal_langs, self.personal_religion, self.sex, self.status])
-        attr_list.extend([self.university_name, self.verified])
+        attr_list.extend([self.personal_political, self.personal_langs, self.personal_religion, self.sex, self.university_name, self.verified])
         return(attr_list)
         
         
-def get_user_info(user_ids):
+def get_user_info(user_ids, group_name):
     api_method = 'users.get'
     get_users_request ={
         'access_token': variables.token,
         'user_ids': user_ids,
         'v': variables.version,
-        'fields': 'bdate, career, city, education, follower_count, sex, status, verified, interests, last_seen, occupation, personal, universities'
+        'fields': 'bdate, career, city, education, followers_count, sex, verified, interests, last_seen, occupation, personal, universities'
     }
-    time.sleep(3)
     response = requests.post(f"{variables.url}/{api_method}", get_users_request)
     json_response = json.loads(response.text)['response']
     for data_string in json_response:
@@ -94,7 +97,6 @@ def get_user_info(user_ids):
         user_data.id = data_string.get('id')
         user_data.is_closed = data_string.get('is_closed')
         user_data.bdate = data_string.get('bdate')
-        # user_data.career = data_string.get('career', {}).get('title')  // Not implemented yet
         user_data.city = data_string.get('city', {}).get('title')
         user_data.followers_count = data_string.get('followers_count')
         user_data.education = data_string.get('education', {}).get('university_name')
@@ -103,14 +105,35 @@ def get_user_info(user_ids):
         user_data.last_seen_platform = data_string.get('last_seen', {}).get('platform')
         user_data.occupation_type = data_string.get('occupation', {}).get('type')
         user_data.personal_political = data_string.get('personal', {}).get('political')
-        # user_data.personal_langs = data_string.get('personal', {}).get('langs') // Not implemented yet
         user_data.personal_religion = data_string.get('personal', {}).get('religion')
         user_data.sex = data_string.get('sex')
-        user_data.status = data_string.get('status')
-        # user_data.university_name = data_string.get('') // Not implemented yet
         user_data.verified = data_string.get('verified')
+        # Multiple data fields parsing. Universities:
+        if 'universities' in data_string:
+            uni_name_list = []
+            for uni in data_string['universities']:
+                uni_name_list.append(uni['name'])
+            user_data.university_name = uni_name_list
+        # Languages:
+        if 'personal' in data_string and 'langs' in data_string['personal']:
+            langs_list = []
+            for lang in data_string['personal']['langs']:
+                langs_list.append(lang)
+            user_data.personal_langs = langs_list
+        # Career:
+        if 'career' in data_string:
+            career_list = []
+            for job in data_string['career']:
+                if 'position' in job:
+                    career_list.append(job['position'])
+            user_data.career = career_list
         attr_list = user_data.attr_to_list()
-        with open(f'users.csv', 'a') as csv_file:
+        # Writing to file
+        with open(f'datasets/{group_name}-list.csv', 'a') as csv_file:
             writer = csv.writer(csv_file)
+            headers = ['id', 'is_closed', 'bdate', 'career', 'city', 'followers_count', 'education', 'interests', 'last_seen_time',
+                        'last_seen_platform', 'occupation', 'political', 'langs', 'religion', 'sex', 'university', 'verified']
+            if os.stat(f'datasets/{group_name}-list.csv').st_size == 0:
+                writer.writerow(headers)
             writer.writerow(attr_list)
         
